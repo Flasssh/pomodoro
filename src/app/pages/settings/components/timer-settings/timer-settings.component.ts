e35b4377d6114a004d94b8b65d8b31c753c53efa
Romain Antunes <romainantunes2003@hotmail.com>
2023-12-07 23:08:30 +0100

    feat: adding timer settings

--- src/app/pages/settings/components/timer-settings/timer-settings.component.ts
+++ src/app/pages/settings/components/timer-settings/timer-settings.component.ts
@@ -21,4 +21,56 @@ export class TimerSettingsComponent implements OnInit {
     }
 
 
+    incrementTimer(amount = 1, timerType: string): void {
+
+        if (this.timer === undefined) {
+            return;
+        }
+
+        // because time is in seconds, we multiply by 60
+        const value = 60 * amount;
+
+        switch (timerType) {
+            case 'pomodoro':
+                this.timer.pomodoro += value;
+                break;
+            case 'shortBreak':
+                this.timer.shortBreak += value;
+                break;
+            case 'longBreak':
+                this.timer.longBreak += value;
+                break;
+        }
+
+        this.settingsService.saveSettingsToLocalStorage();
+    }
+
+    decrementTimer(amount = 1, timerType: string): void {
+        if (this.timer === undefined) {
+            return;
+        }
+
+        // because time is in seconds, we multiply by 60
+        const value = 60 * amount;
+
+        // if the timer is already less than 1 minute, we don't want to decrement it
+        if (this.timer[timerType as keyof typeof this.timer] < value) {
+            return;
+        }
+
+        switch (timerType) {
+            case 'pomodoro':
+                this.timer.pomodoro -= value;
+                break;
+            case 'shortBreak':
+                this.timer.shortBreak -= value;
+                break;
+            case 'longBreak':
+                this.timer.longBreak -= value;
+                break;
+        }
+
+        this.settingsService.saveSettingsToLocalStorage();
+    }
+
 }
